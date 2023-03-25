22b23e74740a15c864696992b99ab74ea4bcf59d
MajorDroolz <maximhutz@gmail.com>
2023-03-24 23:04:33 -0400

    Add documentation to the `HapticBeacon` protocol.

--- apps/ios/GuideDogs/Code/Haptics/HapticBeacon.swift
+++ apps/ios/GuideDogs/Code/Haptics/HapticBeacon.swift
@@ -8,10 +8,15 @@
 
 import CoreLocation
 
+/// A protocol specifying a Beacon that emits haptic feedback based on the direction and location of the user.
 protocol HapticBeacon: WandDelegate {
+    /// Represents the audio player used by the beacon.
     var beacon: AudioPlayerIdentifier? { get }
     
+    ///  Initialize the beacon `at` a certain location.
     init(at: CLLocation)
+    /// Starts the `HapticBeacon` and begin emitting haptic feedback.
     func start()
+    /// Stop the `HapticBeacon` and end emitting haptic feedback.
     func stop()
 }
