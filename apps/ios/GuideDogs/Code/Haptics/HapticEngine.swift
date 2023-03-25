6a6aaa2371913dca9fe58f2411f70a89fad0d6d0
MajorDroolz <maximhutz@gmail.com>
2023-03-24 21:37:46 -0400

    Added documentation for HapticEngine.

--- apps/ios/GuideDogs/Code/Haptics/HapticEngine.swift
+++ apps/ios/GuideDogs/Code/Haptics/HapticEngine.swift
@@ -11,6 +11,7 @@ import CoreHaptics
 
 /// ``HapticEngine`` is a helper or wrapper class for creating and triggering haptic feedback on iOS devices using `CoreHaptics` framework. It provides a unified API for creating and managing feedback generators of three different types: ``UIImpactFeedbackGenerator``, ``UISelectionFeedbackGenerator``, and ``UINotificationFeedbackGenerator``.
 class HapticEngine {
+    /// Contains all loaded `UIFeedbackGenerator`.
     private var generators: [UIFeedbackGenerator] = []
     
     /// ``FeedbackStyle`` is a combined enumeration of the all the types of feedbacks from the main feedback generators.
@@ -67,8 +68,11 @@ class HapticEngine {
         return CHHapticEngine.capabilitiesForHardware().supportsHaptics
     }
     
-    /// Creates a feedback generator for the given style of feedback
-    /// - Parameter style: The style of feedback to create a generator for
+    /// Creates a feedback generator for the given style of feedback.
+    ///
+    /// This adds a new generator to the engine that supports `style` if it is not already supported.
+    ///
+    /// - Parameter style: The style of feedback to create a generator from.
     func setup(for style: FeedbackStyle) {
         guard !generators.contains(where: { $0.supports(style) }) else {
             return
@@ -77,25 +81,31 @@ class HapticEngine {
         generators.append(UIFeedbackGenerator.make(supporting: style))
     }
     
+    /// Creates feedback generators for all given styles of feedback.
+    ///
+    /// For each of the `styles`, a new generator is added to the engine if it is not already supported.
+    ///
+    /// - Parameter styles: An array of the each `FeedbackStyle` to be setup.
     func setup(for styles: [FeedbackStyle]) {
         for style in styles {
             setup(for: style)
         }
     }
     
-    /// Removes all feedback generators and frees up their resources
+    /// Removes all feedback generators from the engine and frees their resources.
     func teardownAll() {
         generators.removeAll()
     }
     
-    /// Removes any the generators which support the given style in order to free up their resources
+    /// Removes all the generators that support `style` and frees their resources.
     /// - Parameter style: The style of feedback generators to remove.
     func teardown(for style: FeedbackStyle) {
         generators.removeAll(where: { $0.supports(style) })
     }
     
-    /// Prepares the generator for the given type of feedback. If a generator does not already
-    /// exist, one will be created and then prepared.
+    /// Prepares the generator for the given type of feedback. If a generator does not already exist, one will be created and then prepared.
+    ///
+    /// It is recommended that you prepare a feedback generator before you call it, to minimize latency while the generator is loading.
     ///
     /// - Parameter style: The style of feedback to prepare to be generated.
     func prepare(for style: FeedbackStyle) {
@@ -105,13 +115,13 @@ class HapticEngine {
         generators.first(where: { $0.supports(style) })?.prepare()
     }
     
-    /// Triggers the appropriate feedback generator to generate feedback. If a generator does not
-    /// already exist, one will not be created by this function. `setup(for:)` or `prepare(for:)`
-    /// should be called first.
+    /// Triggers the appropriate feedback generator to generate feedback.
+    ///
+    /// If a generator does not already exist, one will not be created by this function. You should call `setup(for:)` and `prepare(for:)` before you trigger a generator.
     ///
     /// - Parameters:
-    ///   - style: Style of feedback to generate
-    ///   - intensity: Optional parameter for impact-type styles. Ignored by all other styles
+    ///   - style: Style of feedback to generate.
+    ///   - intensity: Optional parameter for impact-type styles. Ignored by all other styles.
     func trigger(for style: FeedbackStyle, intensity: CGFloat? = nil) {
         guard let generator = generators.first(where: { $0.supports(style) }) else {
             return
