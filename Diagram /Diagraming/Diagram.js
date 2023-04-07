08207fd6b40ad46a8bb04e0e5acd92463d4a102f
charnewman17 <cnewman.2022@gmail.com>
2023-04-07 17:34:48 -0400

    Rest of OSM

--- Diagram /Diagraming/Diagram.js
+++ Diagram /Diagraming/Diagram.js
@@ -1,5 +1,5 @@
+%% ------
 flowchart TD
-
 %%App context
     a[AppContext] --> b[EstimatedLocationDetail]
     b <--> c[SpatialDataCache]
@@ -8,16 +8,24 @@ flowchart TD
 
 %%OSM
 
-g[OSMServiceManager]
-h[OSMServiceProtocol]
-i{ServiceModel}
-j[DebuggingSettingsContext]
-k[Realm Database]
-l[Kubernetes Pods]
-i --> g
-h -->g
-j --> i
-l <--> i
+    g[OSMServiceManager]
+    h[OSMServiceProtocol]
+    i{ServiceModel}
+    j[DebuggingSettingsContext]
+    k[Realm Database]
+    l[Kubernetes Pods]
+    p[The PostGIS Database]
+    v[Linux Machine]
+    i --> g
+    h -->g
+    j --> i
+    l <--> i
+    subgraph .
+    p --> v
+    end
+l --> p
+
+%% ------ 
 
     A[Route Parameters] -->|has| B(RouteWayPoint Parameters)
     B --> C(Marker Parameters)
