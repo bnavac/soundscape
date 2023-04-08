287b63e9896ceb1b50824257851e7dd24bfffddf
MajorDroolz <maximhutz@gmail.com>
2023-04-08 16:08:08 -0400

    Documented the NetworkClient.swift class methods.

--- apps/ios/GuideDogs/Network Client/NetworkClient.swift
+++ apps/ios/GuideDogs/Network Client/NetworkClient.swift
@@ -8,24 +8,43 @@
 
 import Foundation
 
+/// An error type that represents the failure to return data from the network request.
 enum NetworkError: Error {
     case noDataReturned
 }
 
+/// A struct representing the response from the remote server.
 struct NetworkResponse {
+    /// The HTTP response header fields.
     var allHeaderFields: [AnyHashable: Any]
+    /// The HTTP response status code.
     var statusCode: HTTPStatusCode
     
+    /// A static property representing an empty `NetworkResponse`.
     static var empty: NetworkResponse {
         return .init(allHeaderFields: [:], statusCode: .unknown)
     }
 }
 
+/// The `NetworkClient` protocol defines a method to request data from a remote server.
+///
+/// Conforming types should implement the `requestData` method which takes a `URLRequest` as input and returns a tuple of `Data` and `NetworkResponse`.
 protocol NetworkClient {
+    /// Requests data from the remote server using the provided `URLRequest`.
+    /// - Parameters:
+    ///   - request: The request to send to the remote server.
+    /// - Returns: A tuple containing the `Data` returned by the server and the `NetworkResponse`.
+    /// - Throws: An error of type `NetworkError.noDataReturned` if no data is returned by the server.
     func requestData(_ request: URLRequest) async throws -> (Data, NetworkResponse)
 }
 
+/// An implementation of the `NetworkClient` protocol using `URLSession`.
 extension URLSession: NetworkClient {
+    /// Requests data from the remote server using the provided `URLRequest` asynchronously.
+    /// - Parameters:
+    ///   - request: The request to send to the remote server.
+    /// - Returns: A tuple containing the `Data` returned by the server and the `NetworkResponse`.
+    /// - Throws: An error of type `NetworkError.noDataReturned` if no data is returned by the server.
     func requestData(_ request: URLRequest) async throws -> (Data, NetworkResponse) {
         request.log()
         
@@ -74,6 +93,7 @@ extension URLSession: NetworkClient {
 }
 
 extension URLRequest {
+    /// Logs the HTTP method and URL of the `URLRequest`.
     func log() {
         guard let method = httpMethod?.prefix(3) else {
             return
@@ -84,6 +104,10 @@ extension URLRequest {
 }
 
 extension URLResponse {
+    /// Logs the HTTP method, response status code, and URL of the `URLResponse`.
+    ///
+    /// - Parameters:
+    ///   - request: The original `URLRequest` that generated the response.
     func log(request: URLRequest) {
         let responseStatus: HTTPStatusCode
         if let res = self as? HTTPURLResponse {
