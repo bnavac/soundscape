b4358e2815c5bb6076720083b5b7b5ac6b495660
roederdt <123527836+roederdt@users.noreply.github.com>
2023-04-25 18:43:27 -0400

    Commenting OfflineBannerObserver

--- apps/ios/GuideDogs/Code/Notifications/Observers/Large Banner/OfflineBannerObserver.swift
+++ apps/ios/GuideDogs/Code/Notifications/Observers/Large Banner/OfflineBannerObserver.swift
@@ -9,74 +9,74 @@
 import Foundation
 
 class OfflineBannerObserver: PersistentNotificationObserver {
-    
+
     // MARK: Properties
-    
-    weak var delegate: NotificationObserverDelegate?
-    private var offlineState: OfflineState
-    
+
+    weak var delegate: NotificationObserverDelegate? // A delegate for this observer
+    private var offlineState: OfflineState // The current offline state
+
     // MARK: Initialization
-    
+
     init() {
-        self.offlineState = AppContext.shared.offlineContext.state
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(self.onOfflineStateDidChange), name: Notification.Name.offlineStateDidChange, object: nil)
+        self.offlineState = AppContext.shared.offlineContext.state // Initialize the offline state from the shared app context
+
+        NotificationCenter.default.addObserver(self, selector: #selector(self.onOfflineStateDidChange), name: Notification.Name.offlineStateDidChange, object: nil) // Register for notifications when the offline state changes
     }
-    
+
     // MARK: Notifications
-    
+
     @objc
     private func onOfflineStateDidChange(_ notification: Notification) {
         guard let offlineState = notification.userInfo?[OfflineContext.Keys.state] as? OfflineState else {
             return
         }
-        
-        self.offlineState = offlineState
-        
-        delegate?.stateDidChange(self)
+
+        self.offlineState = offlineState // Update the offline state
+
+        delegate?.stateDidChange(self) // Notify the delegate of the state change
     }
-    
+
     // MARK: `NotificationManager`
-    
+
     func notificationViewController(in viewController: UIViewController) -> UIViewController? {
-        guard offlineState != .online else {
+        guard offlineState != .online else { // If the app is online, return nil
             return nil
         }
-        
+
         var notificationViewController: BannerViewController?
-        
+
         if offlineState == .offline {
-            notificationViewController = OfflineBannerViewController(in: viewController)
+            notificationViewController = OfflineBannerViewController(in: viewController) // If the app is offline, show the OfflineBannerViewController
         }
-        
+
         if offlineState == .enteringOnline {
-            notificationViewController = BannerViewController(nibName: "OnlineBanner")
+            notificationViewController = BannerViewController(nibName: "OnlineBanner") // If the app is entering online mode, show the OnlineBannerViewController
         }
-        
+
         // Initialize delegate
-        notificationViewController?.delegate = self
-        
+        notificationViewController?.delegate = self // Set this object as the delegate for the notification view controller
+
         return notificationViewController
     }
-    
+
 }
 
 extension OfflineBannerObserver: BannerViewControllerDelegate {
-    
+
     func didSelect(_ bannerViewController: BannerViewController) {
         guard let offlineBannerViewController = bannerViewController as? OfflineBannerViewController else {
             return
         }
-        
+
         guard let segue = offlineBannerViewController.segue else {
             return
         }
-        
-        delegate?.performSegue(self, destination: segue)
+
+        delegate?.performSegue(self, destination: segue) // Notify the delegate that a segue should be performed
     }
-    
+
     func didDismiss(_ bannerViewController: BannerViewController) {
         // no-op
     }
-    
+
 }
