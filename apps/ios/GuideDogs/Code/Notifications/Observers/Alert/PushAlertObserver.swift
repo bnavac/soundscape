feb1fc32bae728ed37dcc5d10eef6f97b069f770
roederdt <123527836+roederdt@users.noreply.github.com>
2023-04-25 18:39:23 -0400

    Commenting PushAlertObserver

--- apps/ios/GuideDogs/Code/Notifications/Observers/Alert/PushAlertObserver.swift
+++ apps/ios/GuideDogs/Code/Notifications/Observers/Alert/PushAlertObserver.swift
@@ -10,50 +10,54 @@ import Foundation
 import SafariServices
 
 class PushAlertObserver: NotificationObserver {
-    
+
     // MARK: Properties
-    
+
     weak var delegate: NotificationObserverDelegate?
     var didDismiss: Bool = false
     private var pushNotification: PushNotification?
-    
+
     // MARK: Initialization
-    
+
     init() {
+        // Register for push notification received notification
         NotificationCenter.default.addObserver(self,
-                                               selector: #selector(self.pushNotificationReceived),
-                                               name: Notification.Name.pushNotificationReceived,
-                                               object: nil)
+                                            selector: #selector(self.pushNotificationReceived),
+                                            name: Notification.Name.pushNotificationReceived,
+                                            object: nil)
     }
-    
+
     // MARK: Notifications
-    
+
+    // Called when a push notification is received
     @objc
     private func pushNotificationReceived(_ notification: Notification) {
         guard let pushNotification = notification.userInfo?[PushNotificationManager.NotificationKeys.pushNotification] as? PushNotification else {
             return
         }
-        
+
         self.pushNotification = pushNotification
-        
+
         didDismiss = false
-        
+
         delegate?.stateDidChange(self)
     }
-    
+
+    // Resets the current push data when the push alert is shown
     private func didShowPush() {
-        // Reset current push data
         didDismiss = true
         pushNotification = nil
     }
-    
-    // MARK: `NotificationObserver`
-    
+
+    // MARK: `NotificationObserver` implementation
+
+    // Returns a view controller to present the push notification
     func notificationViewController(in viewController: UIViewController) -> UIViewController? {
         guard let pushNotification = pushNotification else {
             return nil
         }
-        
+
+        // Construct the message to show in the alert
         var message: String? {
             if let subtitle = pushNotification.subtitle, let body = pushNotification.body {
                 return subtitle + "\n" + body
@@ -65,21 +69,25 @@ class PushAlertObserver: NotificationObserver {
                 return nil
             }
         }
-        
+
+        // Ensure the notification has text to show
         guard pushNotification.title != nil || message != nil else {
             GDLogPushError("Cannot present push notification in-app alert. Reason: notification does not contain text.")
             return nil
         }
-        
+
+        // Create an alert controller to display the push notification
         let alertController = UIAlertController(title: pushNotification.title,
                                                 message: message,
                                                 preferredStyle: .alert)
-        
+
+        // Add a dismiss action
         let dismissAction = UIAlertAction(title: GDLocalizedString("general.alert.dismiss"), style: .cancel, handler: { [weak self] (_) in
             self?.didShowPush()
         })
         alertController.addAction(dismissAction)
-        
+
+        // If the push notification has a URL, add an action to open it in Safari
         if let urlString = pushNotification.url,
             let url = URL(string: urlString),
             let rootViewController = AppContext.rootViewController {
@@ -94,8 +102,8 @@ class PushAlertObserver: NotificationObserver {
             alertController.addAction(openURLAction)
             alertController.preferredAction = openURLAction
         }
-        
+
         return alertController
     }
-    
+
 }
