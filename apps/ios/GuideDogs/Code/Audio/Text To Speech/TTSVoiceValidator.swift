87cc6344f1b5c8d710238b58a4d546f805a5209c
MajorDroolz <maximhutz@gmail.com>
2023-04-25 21:44:28 -0400

    Added specifications and documentation to the text to speech voice validator.

--- apps/ios/GuideDogs/Code/Audio/Text To Speech/TTSVoiceValidator.swift
+++ apps/ios/GuideDogs/Code/Audio/Text To Speech/TTSVoiceValidator.swift
@@ -9,17 +9,28 @@
 import AVFoundation
 import Combine
 
+/// `TTSVoiceValidator` is a class used to validate a text-to-speech (TTS) voice.
 class TTSVoiceValidator {
+    /// The identifier for the TTS voice being validated.
     let identifier: String
     
+    /// The cancellable object used to cancel the TTS voice validation.
     private var cancellable: AnyCancellable?
     
+    /// Initializes a new `TTSVoiceValidator` instance with the specified TTS voice identifier.
+    /// - Parameter voice: The identifier of the TTS voice to validate.
     init(identifier voice: String) {
         self.identifier = voice
     }
     
+    /// Validates the TTS voice specified during initialization.
+    /// - Returns: A `Future` object that will emit a Boolean value indicating whether the TTS voice is valid or not.
     func validate() -> Future<Bool, Never> {
         return Future { [weak self] promise in
+            // Ensure that voice identifier, AVSpeechSynthesisVoice and
+            // TTSAudioBufferPublisher objects can be created. If any of the
+            // objects cannot be created, immediately resolve the Future with a
+            // value of false.
             guard let id = self?.identifier,
                   let voice = AVSpeechSynthesisVoice(identifier: id),
                   let ttsAudioBufferPublisher = TTSAudioBufferPublisher(voice.name, voiceIdentifier: id) else {
@@ -27,6 +38,10 @@ class TTSVoiceValidator {
                 return
             }
             
+            // Subscribe to the TTSAudioBufferPublisher and collect all emitted
+            // values. If any errors occur during the subscription, resolve the
+            // Future with a value of false. If the subscription completes
+            // successfully, resolve the Future with a value of true.
             self?.cancellable = ttsAudioBufferPublisher.collect().sink(receiveCompletion: { [weak self] (result) in
                 switch result {
                 case .failure: promise(.success(false))
@@ -35,7 +50,7 @@ class TTSVoiceValidator {
                 
                 self?.cancellable = nil
             }, receiveValue: { (_) in
-                // Intentional no-op
+                // We don't need to handle it in any way.
             })
         }
     }
