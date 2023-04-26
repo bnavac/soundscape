c8b3c9b91637d58cac16afc0aeb67d29788dd32f
roederdt <123527836+roederdt@users.noreply.github.com>
2023-04-25 20:17:30 -0400

    Commenting UIDeviceManager

--- apps/ios/GuideDogs/Code/Devices/UIDeviceManager.swift
+++ apps/ios/GuideDogs/Code/Devices/UIDeviceManager.swift
@@ -15,54 +15,40 @@ import UIKit
 import Reachability
 import CocoaLumberjackSwift
 
+// Extend the Notification.Name enumeration to add custom notification names
 extension Notification.Name {
     static let networkConnectionChanged = Notification.Name("GDANetworkConnectionChanged")
     static let uiDeviceStateDidChange = Notification.Name("GDADeviceStateDidChange")
 }
 
+// Define a class that represents the device manager
 class UIDeviceManager: NSObject {
-    
+
+    // Define some keys to use when posting notifications
     struct Keys {
         static let isNetworkAvailable = "GDAIsNetworkAvailableKey"
         static let state = "GDADeviceState"
     }
-    
-    // MARK: Enums
-    
+
+    // Define some enums that represent device state properties
     enum Orientation {
         case horizontal
         case unknown
     }
-    
+
     enum State: String {
         case `default`
         case unknown
     }
 
-    // MARK: Properties
-    
-    // Reachability Properties
-    
-    private let reachability = try? Reachability()
-
-    var isNetworkConnectionAvailable: Bool {
-        guard let reachability = reachability else { return false }
-        return reachability.connection != .unavailable
-    }
-    
-    var isReachableViaWiFi: Bool {
-        guard let reachability = reachability else { return false }
-        return reachability.connection == .wifi
-    }
-    
-    // Device State Properties
-    
+    // Define some device state properties
     private var applicationStateIsActive: Bool = true {
+        // This property observes changes to itself and updates the state property accordingly
         didSet {
             guard oldValue != applicationStateIsActive else {
                 return
             }
-            
+
             if applicationStateIsActive {
                 state = .default
             } else if orientation == .unknown {
@@ -70,13 +56,14 @@ class UIDeviceManager: NSObject {
             }
         }
     }
-    
+
     private var orientation: Orientation = .unknown {
+        // This property observes changes to itself and updates the state property accordingly
         didSet {
             guard oldValue != orientation else {
                 return
             }
-            
+
             if orientation == .horizontal {
                 state = .default
             } else if applicationStateIsActive == false {
@@ -84,79 +71,91 @@ class UIDeviceManager: NSObject {
             }
         }
     }
-    
+
     var state: State = .default {
+        // This property observes changes to itself and posts a notification with the new state
         didSet {
             guard oldValue != state else {
                 return
             }
-            
+
             GDLogAppInfo("Device state: \(state.rawValue)")
-            
+
             let userInfo = [Keys.state: state]
             NotificationCenter.default.post(name: Notification.Name.uiDeviceStateDidChange, object: self, userInfo: userInfo)
         }
     }
-    
-    // Static Properties
-    
+
+    // Define some reachability properties to determine network connection status
+    private let reachability = try? Reachability()
+
+    var isNetworkConnectionAvailable: Bool {
+        guard let reachability = reachability else { return false }
+        return reachability.connection != .unavailable
+    }
+
+    var isReachableViaWiFi: Bool {
+        guard let reachability = reachability else { return false }
+        return reachability.connection == .wifi
+    }
+
+    // Define some static properties for checking battery status
     static var isSimulator: Bool {
         return TARGET_OS_SIMULATOR != 0
     }
-    
+
     private static var batteryState: UIDevice.BatteryState {
         return UIDevice.current.batteryState
     }
-    
+
     private static var batteryLevel: Float {
         return UIDevice.current.batteryLevel
     }
-    
-    // MARK: - Initialization
 
+    // Define an initializer for the device manager
     override init() {
         super.init()
 
-        // Battery configutation
-        
+        // Configure battery monitoring
         UIDevice.current.isBatteryMonitoringEnabled = true
 
+        // Add observers for battery-related notifications and post the current battery status
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(self.batteryStateChanged),
                                                name: UIDevice.batteryStateDidChangeNotification,
-                                               object: nil)
-        
+                                               object: nil
+
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(self.batteryLevelChanged),
                                                name: UIDevice.batteryLevelDidChangeNotification,
                                                object: nil)
-        
+
         // Notify of current battery status
         NotificationCenter.default.post(name: UIDevice.batteryStateDidChangeNotification, object: self)
         NotificationCenter.default.post(name: UIDevice.batteryLevelDidChangeNotification, object: self)
-        
+
         // Network configutation
 
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(self.reachabilityStateChanged),
                                                name: Notification.Name.reachabilityChanged,
                                                object: reachability)
-        
+
         NotificationCenter.default.addObserver(self, selector: #selector(self.onAppWillEnterForeground), name: Notification.Name.appWillEnterForeground, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.onAppDidEnterBackground), name: Notification.Name.appDidEnterBackground, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.onDeviceOrientationDidChange(notification:)), name: Notification.Name.phoneIsFlatChanged, object: nil)
-        
+
         do {
             try reachability?.startNotifier()
         } catch {
             DDLogWarn("Could not start reachability notifier")
         }
     }
-    
+
     deinit {
         reachability?.stopNotifier()
     }
-    
+
     // MARK: Notifications
 
     @objc private func batteryStateChanged(_ notification: NSNotification) {
@@ -166,7 +165,7 @@ class UIDeviceManager: NSObject {
             GDLogBatteryInfo("Battery state: \(UIDeviceManager.batteryState.description)")
         }
     }
-    
+
     @objc private func batteryLevelChanged(_ notification: NSNotification) {
         if UIDeviceManager.isSimulator {
             GDLogBatteryInfo("Battery level: Simulator")
@@ -176,10 +175,10 @@ class UIDeviceManager: NSObject {
             GDLogBatteryInfo("Battery level: " + String(format: "%.0f", UIDeviceManager.batteryLevel*100) + "%")
         }
     }
-    
+
     @objc private func reachabilityStateChanged(_ notification: NSNotification) {
         let reachability = notification.object as! Reachability
-        
+
         if reachability.connection != .unavailable {
             if reachability.connection == .wifi {
                 GDLogNetworkWarn("Network status: WiFi connection")
@@ -189,31 +188,31 @@ class UIDeviceManager: NSObject {
         } else {
             GDLogNetworkWarn("Network status: Offline")
         }
-        
+
         NotificationCenter.default.post(name: Notification.Name.networkConnectionChanged, object: nil, userInfo: [Keys.isNetworkAvailable: isNetworkConnectionAvailable])
     }
-    
+
     @objc
     private func onAppWillEnterForeground() {
         applicationStateIsActive = true
     }
-    
+
     @objc
     private func onAppDidEnterBackground() {
         applicationStateIsActive = false
     }
-    
+
     @objc
     private func onDeviceOrientationDidChange(notification: Notification) {
         guard let isFlat = notification.userInfo?[DeviceMotionManager.Keys.phoneIsFlat] as? Bool else {
             return
         }
-        
+
         if isFlat {
             orientation = .horizontal
         } else {
             orientation = .unknown
         }
     }
-    
+
 }
